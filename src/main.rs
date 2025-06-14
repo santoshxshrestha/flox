fd51751d000e79c71280a7658c6d0f5b6da0c9cb
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-06-14 20:47:59 +0545

    mod

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,8 @@
+mod routes;
 #![allow(unused)]
 use actix_web::{App, HttpResponse, HttpServer, Responder, get, post, web};
 use askama::Template;
 
-#[derive(Template)]
-#[template(path = "home.html")]
-struct Home {
-    rustacean: String,
-}
-
-#[get("/")]
-async fn home() -> impl Responder {
-    let template = Home {
-        rustacean: "Santosh".to_string(),
-    };
-    HttpResponse::Ok()
-        .content_type("text/html")
-        .body(template.render().unwrap())
-}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
