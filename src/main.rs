97977af3548945251d2b6e968a4ba1f83ba3e280
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-06-14 20:34:15 +0545

    wrote basic template

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,26 @@
 #![allow(unused)]
 use actix_web::{App, HttpResponse, HttpServer, Responder, get, post, web};
+use askama::Template;
+
+#[derive(Template)]
+#[template(path = "home.html")]
+struct Home {
+    rustacean: String,
+}
 
 #[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+async fn home() -> impl Responder {
+    let template = Home {
+        rustacean: "Santosh".to_string(),
+    };
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(template.render().unwrap())
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(hello))
+    HttpServer::new(|| App::new().service(home))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
