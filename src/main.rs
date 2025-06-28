f2bd69727deb701f75933e7101dc54cdd76ea079
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-06-28 13:12:50 +0545

    Basic format

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(unused)]
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, post, web};
+
+#[get("/")]
+async fn home() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(home))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
