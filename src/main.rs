7ead90ab941aceac1b918c3319c3d5c5ad697c20
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-06-28 20:50:10 +0545

    Added open port with ip

--- src/main.rs
+++ src/main.rs
@@ -126,7 +126,7 @@ async fn main() -> std::io::Result<()> {
             .service(delete_message)
             .service(Files::new("/static", "static"))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
