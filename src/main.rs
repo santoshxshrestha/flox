fc60a8768425c3b32c9311d84a842fa86286a567
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-07-03 20:23:53 +0545

    new cookkie

--- src/main.rs
+++ src/main.rs
@@ -78,6 +78,7 @@ async fn send_message(
     req: HttpRequest,
 ) -> actix_web::Result<HttpResponse> {
     let mut token = String::new();
+    let mut set_new_cookie = String::new();
     if let Some(cookie) = req.cookie("token") {
         token = cookie.to_string();
     } else {
